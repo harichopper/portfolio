bf81be86354ca09951b8ad079330bd4166baeff2
harichopper <haricdonh@gmail.com>
2024-11-20 19:33:37 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const PORT = 3020;
+const path = require('path');
+
+// Serve the homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Update the path to your HTML file
+});
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: 'https://portfolio-wybb.onrender.com' })); // Update with your actual frontend domain
+
+// MongoDB connection string
+const uri = "mongodb+srv://haricdonh:hari5678@haricluster.0tsnw.mongodb.net/";
+const client = new MongoClient(uri);
+const dbName = "portfolio_website";
+const collectionName = "messages";
+
+app.post('/contact', async (req, res) => {
+    const { Name, email, message } = req.body;
+
+    if (!Name || !email || !message) {
+        return res.status(400).json({ error: 'All fields are required!' });
+    }
+
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        const result = await collection.insertOne({
+            name: Name,
+            email: email,
+            message: message,
+            created_at: new Date()
+        });
+
+        res.status(200).json({ success: 'Message saved successfully!', id: result.insertedId });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Failed to save the message.' });
+    } finally {
+        await client.close();
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
